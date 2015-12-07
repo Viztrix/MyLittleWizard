20939a57a5f16c00b893e75973da5ad90f880fd3
Valdemar Voss <Vixitdk@gmail.com>
2015-12-07 14:12:14 +0100

    tættere på at kunne gå

--- MyLittleWizard/MyLittleWizard/Wizard.cs
+++ MyLittleWizard/MyLittleWizard/Wizard.cs
@@ -8,8 +8,8 @@ namespace MyLittleWizard
 {
     class Wizard : GameObject
     {
-        private bool hasPotionKey, hasFrostKey, hasReachedFrostTower, hasReachedPotionTower;
-
+        private bool hasPotionKey, hasFrostKey, hasReachedFrostTower, hasReachedPotionTower, moving;
+        private Stack<Tile> path;    
         public Wizard(Vector2 position, Vector2 gridPos)
             : base(position, gridPos)
         {
@@ -33,6 +33,21 @@ namespace MyLittleWizard
             }
 
             return new Vector2(0, 0);
+        }
+
+        public override void Update(GameTime gametime)
+        {
+            if (!moving)
+            {
+                moving = true;
+                
+                //path = GameWorld.GameGrid.AStar(GameWorld.GameGrid.Tiles[(int)gridPos.X,(int)gridPos.Y], GameWorld.GameGrid.Tiles[,]);
+            }
+
+
+
+
+            base.Update(gametime);
         }
     }
 }
